88ae44d3ed91fc4bdb4650cddfaf3db4836e8a19
enduserrr <ai.salo@outlook.com>
2024-10-23 13:51:30 +0300

    parsea

--- src/main.c
+++ src/main.c
@@ -38,7 +38,7 @@ static char	*read_fd(int fd)
         return (NULL);
 	}
 	close(fd);
-	printf("From read:\n%s\n", new);
+	// printf("From read:\n%s\n", new);
     return (new);
 }
 
@@ -55,8 +55,8 @@ static int	get_map(char **av, t_map *map_info)
 	free(temp_line);
 	if (map_info->temp_map == NULL)
 		return (write_err("map error 0"), 1);
-	basic_check(map_info);
-	return (0);
+	// validations(map_info);
+	return (validations(map_info));
 }
 
 static int validate_file(char *name)
@@ -80,6 +80,7 @@ int	main(int ac, char **av)
 {
 	static t_map	map_info;
 	int				fd;
+	char			**s;
 
 	if (ac != 2)
         return (exit_error("error: invalid argument count", 1), 1);
@@ -88,6 +89,10 @@ int	main(int ac, char **av)
 		exit(1);
 	if (get_map(av, &map_info))
 		exit(1); /*Add a exit protocol later*/
+	s = map_info.temp_map;
+	put_arr(s);
+	printf("\nx:%lu\ny:%lu\n", map_info.size_x, map_info.size_y);
+	printf("P:%c x:%zu y:%zu\n", map_info.start_orientation, map_info.size_x, map_info.size_y);
 	// gameplay();
 	return(0);
 }
